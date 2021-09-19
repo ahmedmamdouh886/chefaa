14e428e6366f0c772803d743109ff02fc0ec29fe
Ahmed Mamdouh <amamdouh@jobzella.com>
2021-09-19 04:45:13 +0200

    Repo classes names changed.

---
+++ app/Repositories/Pharmacy.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Pharmacy as PharmacyModel;
+use App\Models\Product;
+
+class Pharmacy
+{
+    /**
+     * Pharmacy model instance.
+     *
+     * @var \App\Models\Pharmacy
+     */
+    protected PharmacyModel $pharmacyModel;
+
+    /**
+     * Instantiate a class instance.
+     *
+     * @param \App\Models\Pharmacy
+     */
+    public function __construct(PharmacyModel $pharmacyModel)
+    {
+        $this->pharmacyModel = $pharmacyModel;
+    }
+
+    /**
+     * Paginate all pharmacies.
+     * 
+     * @param int $perPage How many rows you want per page.
+     * @param array $columns What columns you wanna fetch.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function paginate(int $perPage = 10, array $columns = ['*'])
+    {
+        return $this->pharmacyModel->latest()->paginate($perPage, $columns);
+    }
+
+    /**
+     * Find a pharmacy by id.
+     *
+     * @param int $id
+     *
+     * @return \App\Models\Pharmacy
+     */
+    public function findById(int $id, array $columns = ['*'])
+    {
+        return $this->pharmacyModel->findOrFail($id, $columns);
+    }
+
+    /**
+     * Update a pharmacy.
+     *
+     * @param int $id
+     * @param array $data
+     *
+     * @return void
+     */
+    public function update(int $id, array $data): void
+    {
+        $this->findById($id, ['id'])->update($data);
+    }
+
+    /**
+     * Create a new pharamcy.
+     *
+     * @param array $data
+     *
+     * @return void
+     */
+    public function create(array $data): void
+    {
+        $this->pharmacyModel->create($data);
+    }
+
+    /**
+     * Delete a pharamcy.
+     *
+     * @param int $id
+     *
+     * @return void
+     */
+    public function delete(int $id): void
+    {
+        $this->findById($id)->delete();
+    }
+}
---
+++ app/Repositories/Product.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Product as ProductModel;
+use Illuminate\Support\Facades\DB;
+
+class Product
+{
+    /**
+     * Product model instance.
+     *
+     * @var $productModel
+     */
+    protected $productModel;
+
+    /**
+     * Product model instance.
+     *
+     * @var \App\Repositories\PharmacyRepository $pharmacyRepository
+     */
+    protected Pharmacy $pharmacyRepository;
+
+    /**
+     * Instantiate a class instance.
+     *
+     * @param \App\Models\Product $productModel $productModel
+     * @param \App\Repositories\PharmacyRepository $pharmacyRepository
+     */
+    public function __construct(ProductModel $productModel, Pharmacy $pharmacyRepository)
+    {
+        $this->productModel = $productModel;
+        $this->pharmacyRepository = $pharmacyRepository;
+    }
+
+    /**
+     * Paginate all products.
+     *
+     * @param int $perPage How many rows you want per page.
+     * @param array $columns What columns you wanna fetch.
+     * @param array $filters data to filter the products by title or wahtever.
+     * 
+     * @return \Illuminate\Http\Response
+     */
+    public function paginate(int $perPage = 10, array $columns = ['*'], array $filters = [])
+    {
+        return $this->productModel->filter($filters)->latest()->paginate($perPage, $columns);
+    }
+
+    /**
+     * Load relations
+     *
+     * @param array|string $relations some eager loading relations.
+     *
+     * @return $this
+     */
+    public function with(...$relations)
+    {
+        $this->productModel = $this->productModel->with($relations);
+
+        return $this;
+    }
+
+    /**
+     * Find a product by id.
+     *
+     * @param int $id product id.
+     *
+     * @return \App\Models\Product
+     */
+    public function findById(int $id, $columns = ['*'])
+    {
+        return $this->productModel->findOrFail($id, $columns);
+    }
+
+    /**
+     * Update a product.
+     *
+     * @param int $id product id.
+     * @param array $data data to be created.
+     *
+     * @return void
+     */
+    public function update(int $id, array $data): void
+    {
+        $this->findById($id, ['id'])->update($data);
+    }
+
+    /**
+     * Create a new product.
+     *
+     * @param array $data data to be created.
+     *
+     * @return int
+     */
+    public function create(array $data): int
+    {
+        return $this->productModel->create($data)->id;
+    }
+
+    /**
+     * Delete a product.
+     *
+     * @param int $id product id.
+     *
+     * @return void
+     */
+    public function delete(int $id): void
+    {
+        $this->findById($id)->delete();
+    }
+}
