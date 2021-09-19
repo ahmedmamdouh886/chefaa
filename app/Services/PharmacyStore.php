2f145369bb42b5c6b267d88d8523b531889ee59a
Ahmed Mamdouh <amamdouh@jobzella.com>
2021-09-19 04:47:36 +0200

    Pharmacy product store manager, created.

---
+++ app/Services/PharmacyStore.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Services;
+
+use App\Repositories\PharmacyStore as PharmacyStoreRepository;
+
+class PharmacyStore
+{
+    /**
+     * Pharmacy model instance.
+     *
+     * @var \App\Repositories\PharmacyStore $pharmacyStoreRepository
+     */
+    protected PharmacyStoreRepository $pharmacyStoreRepository;
+
+    /**
+     * Instantiate a class instance.
+     *
+     * @param \App\Repositories\PharmacyStore $pharmacyStoreRepository
+     */
+    public function __construct(PharmacyStoreRepository $pharmacyStoreRepository)
+    {
+        $this->pharmacyStoreRepository = $pharmacyStoreRepository;
+    }
+
+    /**
+     * handle pharmacy product store.
+     *
+     * @param int $productId
+     * @param array $pharmaciesProductDetails ['pharmacy_id' => '', 'quanitity' => '', 'price' => '']
+     *
+     * @return void
+     */
+    public function handle(int $productId, array $pharmaciesProductDetails): void
+    {
+        $formattedPharmaciesProductDetails = [];
+        collect($pharmaciesProductDetails)->each(function($singlePharmacyProductDetails) use (&$formattedPharmaciesProductDetails) {
+            $formattedPharmaciesProductDetails[$singlePharmacyProductDetails['pharmacy_id']] = [
+                'price' => $singlePharmacyProductDetails['price'],
+                'quantity' => $singlePharmacyProductDetails['quantity']
+            ];
+        });
+        
+        $this->pharmacyStoreRepository->addOrUpdateProductToMultiplePharmacies($productId, $formattedPharmaciesProductDetails);
+    }
+}
