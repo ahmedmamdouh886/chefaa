07087546c4f299fda3bf54380baf4a68ef63d104
Ahmed Mamdouh <amamdouh@jobzella.com>
2021-09-19 05:14:56 +0200

    Pharmacy and product: HTTP requests tests (Feature test).

---
+++ tests/Feature/PharmacyTest.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Pharmacy;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class PharmacyTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * Test pharmacy index.
+     *
+     * @return void
+     */
+    public function testPharmacyIndex()
+    {
+        Pharmacy::factory(5)->create();
+
+        $this->get('/api/v1/pharmacies')
+        ->assertOk()
+        ->assertJsonStructure([
+            'data' => [[
+                'id',
+                'name',
+            ]],
+            'links' => [
+                'first',
+                'last',
+                'prev',
+                'next',
+            ],
+            'meta' => [
+                'current_page',
+                'from',
+                'last_page',
+                'path',
+                'per_page',
+                'to',
+                'total',
+            ]
+        ]);
+    }
+
+    /**
+     * Test pharmacy show.
+     *
+     * @return void
+     */
+    public function testPharmacyShow()
+    {
+        $pharmacy = Pharmacy::factory()->create([
+            'name' => 'Chefaa pharmacy',
+            'address' => 'Maadi',
+        ]);
+
+        $this->get("/api/v1/pharmacies/{$pharmacy->id}")
+        ->assertOk()
+        ->assertJson([
+            'data' => [
+                'id' => $pharmacy->id,
+                'name' => $pharmacy->name,
+                'address' => $pharmacy->address
+            ],
+        ]);
+    }
+
+    /**
+     * Test store pharmacy.
+     *
+     * @return void
+     */
+    public function testStorePharmacy()
+    {
+        $payload = $this->validPayload();
+
+        $this->post('/api/v1/pharmacies', $payload)
+        ->assertCreated()
+        ->assertJson([
+                'message' => __('messages.created_successfully')
+            ]);
+
+        $this->assertDatabaseHas('pharmacies', $payload);
+    }
+
+    /**
+     * Test update pharmacy.
+     *
+     * @return void
+     */
+    public function testUpdatePharmacy()
+    {
+        $pharmacy = Pharmacy::factory()->create();
+        $payload = $this->validPayload();
+
+        $this->put("/api/v1/pharmacies/{$pharmacy->id}", $payload)
+        ->assertNoContent();
+
+        $this->assertDatabaseHas('pharmacies', $payload);
+        $this->assertEquals($payload['name'], $payload['name']);
+        $this->assertEquals($payload['address'], $payload['address']);
+    }
+
+    /**
+     * Test update pharmacy.
+     *
+     * @return void
+     */
+    public function testDeletePharmacy()
+    {
+        $pharmacy = Pharmacy::factory()->create();
+
+        $this->delete("/api/v1/pharmacies/{$pharmacy->id}")
+        ->assertNoContent();
+
+        $this->assertSoftDeleted('pharmacies', $pharmacy->toArray());
+    }
+
+    /**
+     * Valid payload for storing/updating a resource.
+     *
+     * @return array Valid payload for storing/updating a resource
+     */
+    private function validPayload()
+    {
+        return [
+            'name' => 'Chefaa pharmacy',
+            'address' => 'Maadi',
+        ];
+    }
+}
---
+++ tests/Feature/ProductTest.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Product;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ProductTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * Test product index.
+     *
+     * @return void
+     */
+    public function testProductIndex()
+    {
+        Product::factory(5)->create();
+
+        $this->get('/api/v1/products')
+        ->assertOk()
+        ->assertJsonStructure([
+            'data' => [[
+                'id',
+                'title',
+            ]],
+            'links' => [
+                'first',
+                'last',
+                'prev',
+                'next',
+            ],
+            'meta' => [
+                'current_page',
+                'from',
+                'last_page',
+                'path',
+                'per_page',
+                'to',
+                'total',
+            ]
+        ]);
+    }
+
+    /**
+     * Test product show.
+     *
+     * @return void
+     */
+    public function testProductShow()
+    {
+        $product = Product::factory()->create();
+
+        $this->get("/api/v1/products/{$product->id}")
+        ->assertOk()
+        ->assertJson([
+            'data' => [
+                'id' => $product->id,
+                'title' => $product->title,
+                'description' => $product->description,
+                'pharmacies' => [],
+            ],
+        ]);
+    }
+
+    /**
+     * Test store product.
+     *
+     * @return void
+     */
+    public function testStoreProduct()
+    {
+        $payload = $this->validPayload();
+
+        $this->post('/api/v1/products', $payload)
+        ->assertCreated()
+        ->assertJson([
+                'message' => __('messages.created_successfully')
+            ]);
+
+        $this->assertDatabaseHas('products', $payload);
+    }
+
+    /**
+     * Test update product.
+     *
+     * @return void
+     */
+    public function testUpdateProduct()
+    {
+        $product = Product::factory()->create();
+        $payload = $this->validPayload();
+
+        $this->put("/api/v1/products/{$product->id}", $payload)
+        ->assertNoContent();
+
+        $this->assertDatabaseHas('products', $payload);
+        $this->assertEquals($payload['title'], $payload['title']);
+        $this->assertEquals($payload['description'], $payload['description']);
+    }
+
+    /**
+     * Test update product.
+     *
+     * @return void
+     */
+    public function testDeleteProduct()
+    {
+        $product = Product::factory()->create();
+
+        $this->delete("/api/v1/products/{$product->id}")
+        ->assertNoContent();
+
+        $this->assertSoftDeleted('products', $product->toArray());
+    }
+
+    /**
+     * Valid payload for storing/updating a resource.
+     *
+     * @param  array $overrides
+     * @return array Valid payload for storing/updating a resource
+     */
+    private function validPayload($overrides = [])
+    {
+        return array_merge([
+            'title' => 'Panadol',
+            'description' => 'It\'s a medication used to treat fever and mild to moderate pain.',
+        ], $overrides);
+    }
+}
